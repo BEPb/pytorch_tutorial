2ddefb00e2a46744a906a4762b071ce7387e4b53
BEPb <andrej.marinchenko@gmail.com>
2022-05-03 16:40:17 +0300

    знакомство с тензорами

--- 01_основы/03_gradient.py
+++ 01_основы/03_gradient.py
@@ -77,3 +77,22 @@ o.backward()  # вычисление градиентов
 print("Градиенты: ", x.grad)
 
 
+# Создание тензоров из одного числа
+x = torch.tensor(1., requires_grad=True)
+w = torch.tensor(2., requires_grad=True)
+b = torch.tensor(3., requires_grad=True)
+
+print(x)
+print(w)
+print(b)
+
+# Build a computational graph.
+z = w * x + b    # y = 2 * 1 + 3
+
+# Compute gradients.
+z.backward()
+
+# Print out the gradients.
+print(x.grad)    # x.grad = 2
+print(w.grad)    # w.grad = 1
+print(b.grad)    # b.grad = 1
