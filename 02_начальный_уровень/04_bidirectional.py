1f9b56e43a4dbb51421eb47e4aac03f7c17be978
BEPb <andrej.marinchenko@gmail.com>
2022-05-03 22:56:29 +0300

    двунаправленная рекурентная нейронная сеть

--- 02_начальный_уровень/04_bidirectional.py
+++ 02_начальный_уровень/04_bidirectional.py
@@ -13,10 +13,10 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-# Device configuration
+# Конфигурация оборудования
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Hyper-parameters
+# Гипер-параметры
 sequence_length = 28
 input_size = 28
 hidden_size = 128
@@ -26,17 +26,17 @@ batch_size = 100
 num_epochs = 2
 learning_rate = 0.003
 
-# MNIST dataset
-train_dataset = torchvision.datasets.MNIST(root='../../data/',
+# MNIST данные
+train_dataset = torchvision.datasets.MNIST(root='../data/',
                                            train=True, 
                                            transform=transforms.ToTensor(),
                                            download=True)
 
-test_dataset = torchvision.datasets.MNIST(root='../../data/',
+test_dataset = torchvision.datasets.MNIST(root='../data/',
                                           train=False, 
                                           transform=transforms.ToTensor())
 
-# Data loader
+# загрузка
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size, 
                                            shuffle=True)
@@ -45,55 +45,55 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size, 
                                           shuffle=False)
 
-# Bidirectional recurrent neural network (many-to-one)
+# Двунаправленная рекуррентная нейронная сеть (многие к одному)
 class BiRNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(BiRNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
-        self.fc = nn.Linear(hidden_size*2, num_classes)  # 2 for bidirection
+        self.fc = nn.Linear(hidden_size*2, num_classes)  # 2 для двунаправленного
     
     def forward(self, x):
-        # Set initial states
-        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device) # 2 for bidirection 
+        # Установим начальные состояния
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device) # 2 для двунаправленного
         c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
         
-        # Forward propagate LSTM
-        out, _ = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size*2)
+        # Вперед распространять LSTM
+        out, _ = self.lstm(x, (h0, c0))  # out: размер тензора (batch_size, seq_length, hidden_size*2)
         
-        # Decode the hidden state of the last time step
+        # Декодировать скрытое состояние последнего временного шага
         out = self.fc(out[:, -1, :])
         return out
 
 model = BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
 
 
-# Loss and optimizer
+# Ошибка и оптимизация
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     
-# Train the model
+# Тренировка модели
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         images = images.reshape(-1, sequence_length, input_size).to(device)
         labels = labels.to(device)
         
-        # Forward pass
+        # Прямой проход
         outputs = model(images)
         loss = criterion(outputs, labels)
         
-        # Backward and optimize
+        # Обратный проход и оптимизация
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         
         if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+            print ('Эпоха [{}/{}], Шаг [{}/{}], Ошибка: {:.4f}'
                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-# Test the model
+# Тест модели
 with torch.no_grad():
     correct = 0
     total = 0
@@ -105,7 +105,7 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total)) 
+    print('Проверка точности модели на 10000 тестовых изображений: {} %'.format(100 * correct / total))
 
-# Save the model checkpoint
+# Сохраним контрольную точку модели
 torch.save(model.state_dict(), 'model.ckpt')
